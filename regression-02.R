f88c67f1ccfeb381cf11f5958343dbe347bcd314
Thiago Tavares <thiago.tlopess@gmail.com>
2024-07-01 12:51:39 -0300

    att

---
+++ regression-02.R
@@ -0,0 +1,244 @@
+
+# pacotes uteis
+library(hnp) # pacote para envelope simulado
+library(lmtest) # teste reset
+library(car) # para teste de multicolinearidade (fatores de inflacao de variancia)
+library(tseries) # teste de Jarque-Bera
+
+
+## Importando os dados
+dados<- read.table("data-normal.txt",h=T) 
+## Variaveis:
+# peso (y): peso da criancas ao nascer (gramas)
+# pnatal (x1): variavel binaria que informa se a mae fez (1) ou nao (2) pre-natal
+# pesom (x2): peso da mae (Kg)
+# renda (x3): renda familiar percapta da mae (reais)
+# A ideia eh modelar y em funcao das demais
+
+
+## analise descritiva
+summary(dados) # medidas descritivas
+cor(dados) # correlacao
+plot(dados) # diagrama de dispersao
+
+## ajustando o modelo
+fit <- lm(peso~pnatal+pesom+renda,data=dados) # ajustando o modelo
+summary(fit)
+# Verifique R2, R2-ajustado, teste F e testes t
+# Mas note que soh podemos "acreditar" nesses testes apos analise de 
+# diagnostico e os varios testes estudados. Veja a seguir.
+
+fit_backup<-fit # para guardar esse primeiro fit
+
+##### Analise de influencia
+n<-dim(dados)[1] # tamanho da amostra
+
+# com a seguinte funcao se obtem varias medidas de influencia
+influence.measures(fit)
+
+# Alavancagem
+hatvalues(fit)
+h_bar<-fit$rank / n
+limite<-2*h_bar
+abline(plot(hatvalues(fit),ylab="Alavancagem"), 
+       col="red", h=limite,lty=2)
+which(hatvalues(fit)>limite)
+
+# DFFIT
+dffits(fit)
+limite<-2*sqrt(fit$rank / n)
+abline(plot(dffits(fit),ylab="DFFITS"), 
+       col="red", h=c(-limite,limite),lty=2)
+which(abs(dffits(fit))>limite)
+
+# DFBETA
+dfbetas(fit) # cada beta tem seu DF
+
+dfb1<-dfbetas(fit)[,1]
+dfb2<-dfbetas(fit)[,2]
+dfb3<-dfbetas(fit)[,3]
+dfb4<-dfbetas(fit)[,4]
+
+limite<-2/sqrt(n)
+abline(plot(dfb1,ylab="DFBETA 1"), 
+       col=c("red","blue","red"), h=c(-limite,0,limite),lty=c(2,1,2))
+
+abline(plot(dfb2,ylab="DFBETA 2"), 
+       col=c("red","blue","red"), h=c(-limite,0,limite),lty=c(2,1,2))
+
+# distancia de Cook
+cooks.distance(fit)
+limite<-4/(n-fit$rank )
+abline(plot(cooks.distance(fit),ylab="Distancia de Cook"), 
+       col="red", h=limite,lty=2)
+
+
+# residuo
+residuo <- rstudent(fit) # residuo studentizado
+
+plot(residuo,type='p',pch="+",main="Residuos",xlab="indices") # plota os residuos do modelo
+abline(h=c(-2,0,2),lty=3) # inclui linhas horizontais no grafico
+
+which(abs(residuo)>3)
+
+hist(residuo) # histograma dos residuos
+
+# envelope simulado baseado nos residuos studentizados
+hnp(fit,resid.type="student",halfnormal = F) # envelope simulado 
+
+## A observacao 45 eh bastante influente. Vamos investigar:
+dados[45,]
+summary(dados)
+boxplot(peso~pnatal, data=dados)
+# Ha algo muito estranho nesse individuo 45. Uma das rendas mais altas com o menor peso da crianca.
+# Vamos retira-lo dos dados, pois e reestimar.
+
+
+# Retirando observacao possivelmente influente e reestimando 
+
+dados45<-dados[-45,]
+
+## ajustando o modelo
+fit <- lm(peso~pnatal+pesom+renda,data=dados45) # ajustando o modelo
+summary(fit)
+# Verifique R2, R2-ajustado, teste F e testes t
+# Mas note que soh podemos "acreditar" nesses testes apos analise de 
+# diagnostico e os varios testes estudados. Veja a seguir.
+
+summary(fit_backup) 
+
+# Comparando os dois modelos, perceba que muda muito. Ou seja, 
+# a observacao 45 eh realmente influente.
+
+##### Analise de influencia
+n<-dim(dados45)[1] # tamanho da amostra
+
+# com a seguinte funcao se obtem varias medidas de influencia
+influence.measures(fit)
+
+# Alavancagem
+hatvalues(fit)
+h_bar<-fit$rank / n
+limite<-2*h_bar
+abline(plot(hatvalues(fit),ylab="Alavancagem"), 
+       col="red", h=limite,lty=2)
+
+# DFFIT
+dffits(fit)
+limite<-2*sqrt(fit$rank / n)
+abline(plot(dffits(fit),ylab="DFFITS"), 
+       col="red", h=c(-limite,limite),lty=2)
+
+
+# DFBETA
+dfbetas(fit) # cada beta tem seu DF
+
+dfb1<-dfbetas(fit)[,1]
+dfb2<-dfbetas(fit)[,2]
+dfb3<-dfbetas(fit)[,3]
+dfb4<-dfbetas(fit)[,4]
+
+limite<-2/sqrt(n)
+abline(plot(dfb1,ylab="DFBETA 1"), 
+       col=c("red","blue","red"), h=c(-limite,0,limite),lty=c(2,1,2))
+
+abline(plot(dfb2,ylab="DFBETA 2"), 
+       col=c("red","blue","red"), h=c(-limite,0,limite),lty=c(2,1,2))
+
+
+# distancia de Cook
+cooks.distance(fit)
+limite<-4/(n-fit$rank )
+abline(plot(cooks.distance(fit),ylab="Distancia de Cook"), 
+       col="red", h=limite,lty=2)
+
+
+# residuo
+residuo <- rstudent(fit) # residuo studentizado
+
+plot(residuo,type='p',pch="+",main="Residuos",xlab="indices") # plota os residuos do modelo
+abline(h=c(-2,0,2),lty=3) # inclui linhas horizontais no grafico
+
+hist(residuo) # histograma dos residuos
+
+# envelope simulado baseado nos residuos studentizados
+hnp(fit,resid.type="student",halfnormal = F) # envelope simulado 
+
+
+# A observacao 43 apareceu "levemente" em algumas medidas. Mas eh possivel 
+# verificar que sua exclusao nao influencia no ajuste. 
+# Envelope estah muito bom e nao ha fortes evidencias de algo incorreto. Entao seguimos.
+
+
+
+
+##### Testando as suposições do modelo
+
+## [S0] O modelo estah corretamente especificado
+## [S1] A media dos erros eh zero
+## [s2] Homoscedasticidade dos erros
+## [S3] Nao autocorrelacao 
+## [S4] Ausencia de Multicolinearidade
+## [S5] Normalidade dos erros
+
+## Obs.: Para testes de hipoteses, se p-value < alpha (5%) 
+## entao rejeita a hipotese nula (H0)
+
+## Testa [S0]
+## Teste RESET de especificacao
+## H0: O modelo estah corretamente especificado
+resettest(fit)
+
+## Testa [S1]
+## Teste t para a média dos errros
+## H0: média dos erros eh igual a zero
+t.test(resid(fit),mu=0,alternative="two.sided")
+
+## Testa [s2]
+## Teste de Bressch-Pagan (Koenker) de Heteroscedasticidade
+## H0: erros sao homoscedasticos
+bptest(fit, studentize = TRUE)
+
+## Testa [S3]
+## Teste de Durbin-Watson de autocorrelacao
+## H0: : Nao hah autocorrelacao 
+dwtest(fit)
+acf(rstudent(fit))
+
+## Testa [S4]
+## Usa Fatores de Inflacao de Variancia para detectar multicolinearidade
+## Regra de bolso: vif > 10 indica multicolinearidade. vif=1 seria o ideal.
+vif(fit)
+
+## Testa [S5]
+## Teste Jarque-Bera de Normalidade
+## H0: Os erros possuem distribuicao normal
+jarque.bera.test(resid(fit))
+
+
+
+
+
+## Agora com o modelo checado, com boas evidencias de que as suposicoes estao 
+## satisfeitas, eh possivel fazer inferencias e interpretacoes. 
+summary(fit)
+# Quanto cada covariavel influencia na media de y?
+# O quanto a mae ter feito pre-natal influencia no peso da crianca (em media)?
+
+## Para fazer predicao, fazemos
+predict(fit) # valores preditos na amostra usada na estimacao
+
+# Predicao com novos dados
+novos_dados <- data.frame(cbind(
+  pnatal=1, pesom=70, renda=1000
+))
+predict(fit, newdata=novos_dados) # com novos valores (fora da amostra)
+
+## Obs. Se seu banco de dados tiver muitas covariaveis candidatas, 
+## voce pode utilizar a funcao step() para selecionar, via AIC, 
+## um modelo candidato mais enxuto. 
+
+step(fit) # seleciona modelo baseado no AIC e stepwise
+# neste caso o modelo eh ok e o step nao excluiu covariaveis
+
+# Obs.: dados simulados -> pode/deve não representar a realidade
---
+++ script.R
@@ -0,0 +1,189 @@
+library(hnp) # pacote para envelope simulado
+library(lmtest) # teste reset
+library(car) # para teste de multicolinearidade (fatores de inflacao de variancia)
+library(tseries) # teste de Jarque-Bera
+library(gtsummary)
+
+
+
+#data set
+dados<- read.table("dados-trabalho1.txt",h=T) 
+View(dados)
+
+summary(dados)
+cor(dados) # ha multicolinearidade entre x3 e x4
+plot(dados)
+
+
+## ajustando o modelo
+fit <- lm(y~x1+x2+x3+x4+x5+x6+x7,data=dados) # ajustando o modelo
+summary(fit)
+
+tbl_regression(fit)
+#teste F: Pelo teste F se rejeitou HO, ou seja, pelo menos um beta é diferente de zero.
+#teste t: para o modelo em estudo x1 e x6 foram significativos para o modelo, visto que o p-valor foi menor que 0.05
+
+##TESTE DAS SUPOSIÇÕES DO MODELO
+
+## Testa [S0]
+## Teste RESET de especificacao
+## H0: O modelo esta corretamente especificado
+
+resettest(fit) # como p-valor > alfa, então não se rejeita H0 e o modelo esta corretamente especifcado
+
+## Testa [S1]
+## Teste t para a média dos erros
+## H0: média dos erros eh igual a zero
+t.test(resid(fit),mu=0,alternative="two.sided") # como o p-valor foi de 1 > que alfa, entao nao se rejeita H0
+                                                # média dos erros é igual a 0 
+
+## Testa [s2]
+## Teste de Bressch-Pagan (Koenker) de Heteroscedasticidade
+## H0: erros sao homoscedasticos
+bptest(fit, studentize = TRUE) #como o p-valor foi de 0.07121 > 0.05, entao nao se rejeita H0, logo o erros são homoscedasticos
+
+
+## Testa [S3]
+## Teste de Durbin-Watson de autocorrelacao
+## H0: : Nao hah autocorrelacao 
+dwtest(fit)
+acf(rstudent(fit))
+
+## Testa [S4]
+## Usa Fatores de Inflacao de Variancia para detectar multicolinearidade
+## Regra de bolso: vif > 10 indica multicolinearidade. vif=1 seria o ideal.
+vif(fit) #Através dos vifs podemos obsevar que x1, x2, x6 e x7 temos o ideal
+
+## Testa [S5]
+## Teste Jarque-Bera de Normalidade
+## H0: Os erros possuem distribuicao normal
+jarque.bera.test(resid(fit)) # como p-valor > que 0.05, não rejeitamos H0
+
+
+## Agora com o modelo checado, com boas evidencias de que as suposicoes estao 
+## satisfeitas, eh possivel fazer inferencias e interpretacoes. 
+summary(fit)
+
+
+## Para fazer predicao, fazemos
+predict(fit) # valores preditos na amostra usada na estimacao
+
+# Predicao com novos dados
+novos_dados <- data.frame(cbind( ))
+predict(fit, newdata=novos_dados) # com novos valores (fora da amostra)
+
+
+
+##### ANALISE DE INFLUENCIA
+n<-dim(dados)[1] # tamanho da amostra
+
+# com a seguinte funcao se obtem varias medidas de influencia
+influence.measures(fit)  # LINHAS COM POSSIVEIS INFLUENCIAS 18,23,64,63
+
+# Alavancagem
+hatvalues(fit)
+h_bar<-fit$rank / n
+limite<-2*h_bar
+abline(plot(hatvalues(fit),ylab="Alavancagem"), 
+       col="red", h=limite,lty=2)
+which(hatvalues(fit)>limite) # potencialmente influentes e podem merecer uma 
+                             #análise mais detalhada para verificar se são outliers ou se têm um impacto significativo no modelo ajustado.
+
+# DFFIT
+dffits(fit)
+limite<-2*sqrt(fit$rank / n)
+abline(plot(dffits(fit),ylab="DFFITS"), 
+       col="red", h=c(-limite,limite),lty=2)
+which(abs(dffits(fit))>limite)
+
+# DFBETA
+dfbetas(fit) # cada beta tem seu DF
+
+dfb1<-dfbetas(fit)[,1]
+dfb2<-dfbetas(fit)[,2]
+dfb3<-dfbetas(fit)[,3]
+dfb4<-dfbetas(fit)[,4]
+dfb5<-dfbetas(fit)[,5]
+dfb6<-dfbetas(fit)[,6]
+dfb7<-dfbetas(fit)[,7]
+
+limite<-2/sqrt(n)
+abline(plot(dfb1,ylab="DFBETA 1"), 
+       col=c("red","blue","red"), h=c(-limite,0,limite),lty=c(2,1,2))
+
+abline(plot(dfb2,ylab="DFBETA 2"), 
+       col=c("red","blue","red"), h=c(-limite,0,limite),lty=c(2,1,2))
+
+abline(plot(dfb3,ylab="DFBETA 3"), 
+       col=c("red","blue","red"), h=c(-limite,0,limite),lty=c(2,1,2))
+
+abline(plot(dfb4,ylab="DFBETA 4"), 
+       col=c("red","blue","red"), h=c(-limite,0,limite),lty=c(2,1,2))
+
+abline(plot(dfb5,ylab="DFBETA 5"), 
+       col=c("red","blue","red"), h=c(-limite,0,limite),lty=c(2,1,2))
+
+abline(plot(dfb6,ylab="DFBETA 6"), 
+       col=c("red","blue","red"), h=c(-limite,0,limite),lty=c(2,1,2))
+
+abline(plot(dfb7,ylab="DFBETA 7"), 
+       col=c("red","blue","red"), h=c(-limite,0,limite),lty=c(2,1,2))
+
+# distancia de Cook
+cooks.distance(fit)
+limite<-4/(n-fit$rank )
+abline(plot(cooks.distance(fit),ylab="Distancia de Cook"), 
+       col="red", h=limite,lty=2)
+
+
+# residuo
+residuo <- rstudent(fit) # residuo studentizado
+
+plot(residuo,type='p',pch="+",main="Residuos",xlab="indices") # plota os residuos do modelo
+abline(h=c(-2,0,2),lty=3) # inclui linhas horizontais no grafico
+
+which(abs(residuo)>3)
+
+hist(residuo) # histograma dos residuos
+
+# envelope simulado baseado nos residuos studentizados
+hnp(fit,resid.type="student",halfnormal = F) # envelope simulado 
+                                            #Acima no gráfico do envelope dos resíduos, 
+                                            #temos eles "studentizados" e ordenados. 
+                                            #Esse envelope são bandas de confiança e esperamos que pelo menos 90% dos resíduos estejam entre essas bandas. Essas bandas são feitas através de simulações de Monte Carlo. 
+                                            #Esse envelope indica, principalmente, que a suposição distribucional está correta. Ou seja, a distribuição normal é correta para modelar esses dados.
+
+
+
+# Pelo VIF , temos que remover x3 e x4, porém temos x4 possui uma correlaçao maior com y do que x3 coom y
+# e tambem temos que pela distancia de cook a observacao 23 e a mais influente 
+
+
+
+
+
+dados23<-dados[-23,]
+#head(dados23, 25)
+# remocao de x3 divido a alta a correlacao alta com x4
+fit2<-lm(y~x1+x2+x4+x5+x6+x7,data=dados)
+
+summary(fit2)
+
+#tenho que x1, x4 e x6 foram significativos
+n<-dim(dados23)[1] # tamanho da amostra
+
+
+
+## Obs. Se seu banco de dados tiver muitas covariaveis candidatas, 
+## voce pode utilizar a funcao step() para selecionar, via AIC, 
+## um modelo candidato mais enxuto. 
+
+step(fit) # seleciona modelo baseado no AIC e stepwise
+# neste caso o modelo eh ok e o step nao excluiu covariaveis
+
+# Obs.: dados simulados -> pode/deve não representar a realidade
+
+
+
+
+
